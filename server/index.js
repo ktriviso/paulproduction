5903d91b18fcbf7296e4e2ab46d25bffc0d15313
krista triviso <krista.triviso91@gmail.com>
2017-12-30 20:47:04 -0500

    restructured app

---
+++ server/index.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const nodemailer = require('nodemailer')
+const path = require('path')
+const bodyparser = require('body-parser')
+const app = express()
+
+const emailConfig = {
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    user: 'krista.triviso91@gmail.com',
+    password: 'arctica03'
+}
+
+const transporter = nodemailer.createTransport({
+    host: emailConfig.host,
+    port: emailConfig.port,
+    secure: emailConfig.secure,
+    auth: {
+        user: emailConfig.user,
+        pass: emailConfig.password
+    }
+})
+
+app.set('port', (process.env.PORT || 3001))
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({
+    extended: true
+}))
+
+app.use(function(request, response, next){
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader('Cache-Control', 'no-cache')
+    next()
+})
+
+app.use(express.static(path.resolve(__dirname, '../client/build')))
+app.get('*', function(request, response){
+    response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+})
+app.post('/sendEmail', function(request, response){
+    console.log(request.body)
+    const data = request.body.data
+    const emailInfo = {
+        from: data.email,
+        to: emailConfig.user,
+        subject: 'You have a new email from ' + data.name + ' your website',
+        text: `${data.message}, ${data.number}`,
+        html: `${data.message}, ${data.number}`
+    }
+    let responseStatus
+    transporter.sendMail(emailInfo, function(error, info){
+        if(error){
+            responseStatus = false
+            console.log(error)
+        }else {
+            responseStatus = true
+            console.log(info.messageId, info.response)
+        }
+    })
+    return response.json({
+        success: responseStatus
+    })
+})
+
+app.listen(app.get('port'), function(){
+    console.log('server running on: ' + app.get('port'))
+})
+
+module.exports = app
